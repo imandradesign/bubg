6123eb6fa2bb9a64cf6f79f880c3fb9deca9cd8a
Imandra McKenzie <imckenzie@squarespace.com>
2023-10-17 13:45:36 -0700

    update button and add icon

--- ts/scripts.ts
+++ ts/scripts.ts
@@ -1,45 +1,45 @@
 function Player() {
-    this.name = "";
-    this.strikes = 0;
-    this.team = null;
+  this.name = "";
+  this.strikes = 0;
+  this.team = null;
 }
 
 // Define the function to fetch player names
 async function fetchPlayerNames(): Promise<void> {
-    try {
-      const response = await fetch('https://docs.google.com/spreadsheets/d/1G97cC4k_gf1gKlNIJ56X2cAUJl9K5lbTzozbCZSZnwA/gviz/tq?tqx=out:csv');
-      const data = await response.text();
-      const allPlayers = data.split('\n');
-  
-      // Clear the existing list items
-      const playerListElement = document.getElementById('player-list');
-      if (playerListElement) {
-        playerListElement.innerHTML = '';
-      }
-  
-      // Add the fetched players to the list
-      allPlayers.forEach(player => {
-        var newPlayer = new Player();
-        newPlayer.name = player;
+  try {
+    const response = await fetch(
+      "https://docs.google.com/spreadsheets/d/1G97cC4k_gf1gKlNIJ56X2cAUJl9K5lbTzozbCZSZnwA/gviz/tq?tqx=out:csv"
+    );
+    const data = await response.text();
+    const allPlayers = data.split("\n");
 
-        const playerListElement = document.getElementById('player-list');
-        const nameItem = document.createElement('li');
-
-        nameItem.textContent = newPlayer.name;
-        
-        if (playerListElement) {
-            playerListElement.appendChild(nameItem);
-        }
-      });
-  
-    } catch (error) {
-      console.error('An error occurred:', error);
+    // Clear the existing list items
+    const playerListElement = document.getElementById("player-list");
+    if (playerListElement) {
+      playerListElement.innerHTML = "";
     }
+
+    // Add the fetched players to the list
+    allPlayers.forEach((player) => {
+      var newPlayer = new Player();
+      newPlayer.name = player;
+
+      const playerListElement = document.getElementById("player-list");
+      const nameItem = document.createElement("li");
+
+      nameItem.textContent = newPlayer.name;
+
+      if (playerListElement) {
+        playerListElement.appendChild(nameItem);
+      }
+    });
+  } catch (error) {
+    console.error("An error occurred:", error);
   }
-  
-  // Attach the fetch function to button click
-  const fetchButton = document.getElementById('fetch-button');
-  if (fetchButton) {
-    fetchButton.addEventListener('click', fetchPlayerNames);
-  }
-  
+}
+
+// Attach the fetch function to button click hopefully
+const fetchButton = document.getElementById("fetch-button");
+if (fetchButton) {
+  fetchButton.addEventListener("click", fetchPlayerNames);
+}
