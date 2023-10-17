2c7ab59259a1092d39cd46e4c2078b1425f3aff5
Imandra McKenzie <imckenzie@squarespace.com>
2023-10-17 14:13:45 -0700

    add DOMContentLoaded

--- ts/scripts.ts
+++ ts/scripts.ts
@@ -1,48 +1,50 @@
-function Player() {
-  this.name = "";
-  this.strikes = 0;
-  this.team = null;
-}
-
-// Define the function to fetch player names
-async function fetchPlayerNames(): Promise<void> {
-  try {
-    const response = await fetch(
-      "https://docs.google.com/spreadsheets/d/1G97cC4k_gf1gKlNIJ56X2cAUJl9K5lbTzozbCZSZnwA/gviz/tq?tqx=out:csv"
-    );
-    const data = await response.text();
-    const allPlayers = data.split("\n");
-
-    // Clear the existing list items
-    const playerListElement = document.getElementById("player-list");
-    if (playerListElement) {
-      playerListElement.innerHTML = "";
-    }
-
-    // Add the fetched players to the list
-    allPlayers.forEach((player) => {
-      var newPlayer = new Player();
-      newPlayer.name = player;
-      console.log("New player: ", player);
-
-      const playerListElement = document.getElementById("player-list");
-      const nameItem = document.createElement("li");
-
-      nameItem.textContent = newPlayer.name;
-
-      if (playerListElement) {
-        console.log("player-list ID exists.");
-        playerListElement.appendChild(nameItem);
-      }
-    });
-  } catch (error) {
-    console.log("An error occurred:", error);
+document.addEventListener("DOMContentLoaded", function () {
+  function Player() {
+    this.name = "";
+    this.strikes = 0;
+    this.team = null;
   }
-}
 
-// Attach the fetch function to button click hopefully
-const fetchButton = document.getElementById("fetch-button");
-if (fetchButton) {
-  fetchButton.addEventListener("click", fetchPlayerNames);
-  console.log("Button Clicked.");
-}
+  // Define the function to fetch player names
+  async function fetchPlayerNames(): Promise<void> {
+    try {
+      const response = await fetch(
+        "https://docs.google.com/spreadsheets/d/1G97cC4k_gf1gKlNIJ56X2cAUJl9K5lbTzozbCZSZnwA/gviz/tq?tqx=out:csv"
+      );
+      const data = await response.text();
+      const allPlayers = data.split("\n");
+
+      // Clear the existing list items
+      const playerListElement = document.getElementById("player-list");
+      if (playerListElement) {
+        playerListElement.innerHTML = "";
+      }
+
+      // Add the fetched players to the list
+      allPlayers.forEach((player) => {
+        var newPlayer = new Player();
+        newPlayer.name = player;
+        console.log("New player: ", player);
+
+        const playerListElement = document.getElementById("player-list");
+        const nameItem = document.createElement("li");
+
+        nameItem.textContent = newPlayer.name;
+
+        if (playerListElement) {
+          console.log("player-list ID exists.");
+          playerListElement.appendChild(nameItem);
+        }
+      });
+    } catch (error) {
+      console.log("An error occurred:", error);
+    }
+  }
+
+  // Attach the fetch function to button click hopefully
+  const fetchButton = document.getElementById("fetch-button");
+  if (fetchButton) {
+    fetchButton.addEventListener("click", fetchPlayerNames);
+    console.log("Button Clicked.");
+  }
+});
